46d54169ed747cf03ebff0460ea47d1517e344df
ranaberi <ranaberi25@gmail.com>
2022-05-09 11:25:55 +0300

    Positive_Words_Count

--- NLP.py
+++ NLP.py
@@ -6,3 +6,22 @@ def strip_punctuation(word):
         if c not in punctuation_chars:
             new_word= new_word+c
     return new_word
+
+# list of positive words to use
+positive_words = []
+with open("positive_words.txt") as pos_f:
+    for lin in pos_f:
+        if lin[0] != ';' and lin[0] != '\n':
+            positive_words.append(lin.strip())
+
+def get_pos(sentences):
+    lower_string=sentences.lower()                    ## As the list of positive words are in lower case
+    str_punc_removed=strip_punctuation(lower_string)  ##For Punctuation Removal
+    splitted_string=str_punc_removed.split()          ## For splitting the sentence into words
+    
+    
+    pos_count=0
+    for w in splitted_string:
+        if w in positive_words:
+            pos_count=pos_count+1
+    return pos_count
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # NLP-twitter_sentiment_analysis
+
+A sentiment classifier, which detects how positive or negative each tweet is.A csv file is created, which contains columns for the Number of Retweets, Number of Replies, Positive Score (which is how many happy words are in the tweet), Negative Score (which is how many angry words are in the tweet), and the Net Score for each tweet. At the end, a graph of the Net Score vs Number of Retweets is produced.
